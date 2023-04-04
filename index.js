f9cfdff13f7919a7929331ea651edfbf0d9846f2
riccardoconte7904 <riccardoconte7904.3@gmail.com>
2023-04-04 08:16:35 +0200

    Backend: Refactor

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ passport.serializeUser(function(user, done) {
 passport.deserializeUser(function(user, done) {
     done(null, user)
 })
-
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
@@ -63,24 +62,6 @@ app.get('/me', (req, res) => {
 })
 
 
+
 //Server listen
 app.listen(process.env.PORT, () => console.log('\x1b[32m[Express] Server listening on port ' + process.env.PORT + "\x1b[0m"))
-
-
-
-//DB Connection
-// const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'pra_test',
-//     database: 'pra_test'
-// })
-// db.connect((err) => {
-//     if(err) {
-//         console.log("\x1b[31m[MySQL2] Unable to establish database connection!" + "\x1b[0m")
-//         console.log("\x1b[31m[MySQL2] Error code: " + err.code + "\x1b[0m")
-//         console.log("\x1b[31m[MySQL2] " + err.sqlMessage + "\x1b[0m")
-//         return
-//     }
-//     console.log("\x1b[32m[MySQL2] Database server connection established\x1b[0m")
-// })
